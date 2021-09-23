82631bb74ca5a603ea13aa92c1db08bb95da22b0
Saarthak-Slathia <saarthak.slathia@gmail.com>
2021-09-23 12:33:39 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Landing from "./components/Landing";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Landing />
+              <Footer />
+            </Route>
+            <Route exact path="/home">
+              <Alert alert={alert} />
+              <Home showAlert={showAlert} />
+              <Footer />
+            </Route>
+            <Route exact path="/about">
+              <Alert alert={alert} />
+              <About />
+            </Route>
+            <Route exact path="/login">
+              <Alert alert={alert} />
+              <Login showAlert={showAlert} />
+            </Route>
+            <Route exact path="/signup">
+              <Alert alert={alert} />
+              <Signup showAlert={showAlert} />
+            </Route>
+          </Switch>
+        </Router>
+      </NoteState>
+    </>
   );
-}
+};
 
 export default App;
